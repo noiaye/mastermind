9d8ea1ab7f9ab1ae59b50f829ef36df0828d885c
blob <130174758+BlobCybersecurity@users.noreply.github.com>
2026-07-01 10:32:27 +0200

    Fix hint loop looking at wrong values

--- lib/game.rb
+++ lib/game.rb
@@ -3,29 +3,41 @@ require_relative 'player'
 class GameClass
   def initialize
     @current_player_id = 1
-
-    @players = [PlayerClass.new(self, 'human_player'), ComputerPlayer.new(self, 'computer_player')]
+    @players = [HumanPlayer.new(self, 'human_player'), ComputerPlayer.new(self, 'computer_player')]
   end
   attr_accessor :players, :current_player_id
 
-  def current_player
-    players[current_player_id]
+  def computer_player
+    players[1]
+  end
+
+  def human_player
+    players[0]
   end
 
   def switch_players
     self.current_player_id = other_player_id
   end
 
-  def other_player_id
-    1 - current_player_id
-  end
-
   def computer_play_against_human
-    creation = current_player.randomizecolors 
-      # guess = guess method
-      # break if correctly guessed
-      # other wise we give a hint
+    creation = computer_player.randomizecolors
+    # puts creation, 'Code'
+    loop do
+      computer_player.display_colors
+      guess = human_player.guess
+      amount = 0
+      if guess == creation
+        puts 'You guessed the code correctly! Well done.'
+        break
+      elsif guess != creation && amount <= 12
+        amount += 1
+        puts 'Incorrectly guessed but here is a hint mate: '
+        computer_player.give_hint(guess, creation)
+      end
     end
   end
 end
 
+game = GameClass.new
+# game.computer_play_against_human
+game.computer_play_against_human
--- lib/player.rb
+++ lib/player.rb
@@ -17,14 +17,36 @@ class ComputerPlayer < PlayerClass
     new_array
   end
 
+  def display_colors
+    puts 'red blue white yellow green pink black orange brown'
+  end
+
   def give_hint(colors_guessed_array, secret_code_array)
-    colors_guessed_array.each do |color|
-      puts "U got the #{color} right" if secret_code_array.include?(color)
+    colored_peg = 0
+    white_peg = 0
+    colors_guessed_array.each_with_index do |i, v|
+      secret_code_array.each_with_index do |y, x|
+        if i == y && v == x
+          colored_peg += 1
+        elsif i == y && v != x
+          white_peg += 1
+        end
+      end
     end
+    puts "Colored peg: #{colored_peg}, White Peg: #{white_peg}"
   end
 end
 
 class HumanPlayer < PlayerClass
+  def guess
+    loop do
+      puts 'What is your first guess? Hint: The code is 4 colors long'
+      answer = gets.chomp.split(' ')
+      return answer if answer.length == 4
+
+      puts 'Invalid input'
+    end
+  end
 end
 
 # Todo Define Human PLayer guess method so it can interact with game rb
