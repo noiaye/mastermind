62e3b790c47b16d540c7324deb4a6af0a3841cc2
blob <130174758+BlobCybersecurity@users.noreply.github.com>
2026-07-23 23:10:30 +0200

    Imlement basic play game loop and remove uneccesarry.

--- lib/helpers/computer_algorithm_guess.rb
+++ lib/helpers/computer_algorithm_guess.rb
@@ -1,62 +1,56 @@
 # frozen_string_literal: true
 
-require 'pry-byebug'
-computer_guess = %w[red red purple yellow]
-player_creation = %w[red red green blue]
-
 # Some helper functions for the computer_guesser
-# module ComputerAlgorithm
-def exact_values(computer_guess, player_creation)
-  binding.pry
-  filter_array = []
-  algorithm_array = []
-  p 'hi'
-  computer_guess.each_with_index do |v, i|
-    player_creation.each_with_index do |x, y|
-      next unless v == x && i == y
+module ComputerAlgorithm
+  def exact_values(computer_guess, player_creation, filter_array, algorithm_array)
+    computer_guess.each_with_index do |v, i|
+      player_creation.each_with_index do |x, y|
+        next unless v == x && i == y
 
-      p 'e'
-      break if both_qualified(computer_guess, player_creation, filter_array) == false
+        break if both_qualified(computer_guess, player_creation, filter_array) == false
 
-      p '2'
-
-      algorithm_array[i] = 1
-      filter_array.push([i, y])
+        algorithm_array[i] = 1
+        filter_array.push([i, y])
+      end
     end
   end
-  [filter_array, algorithm_array]
-end
 
-# def computer_guess_qualified(computer_guess, filter_array)
-#   computer_guess.each_with_index do |_v, i|
-#     break if filter_array.any? { |v| v[0] == i }
+  def dif_index(computer_guess, player_creation, filter_array, algorithm_array)
+    computer_guess.each_with_index do |v, i|
+      player_creation.each_with_index do |x, y|
+        next unless v == x && i != y
 
-#     p 'true'
-#     true
-#   end
-#   false
-# end
-def both_qualified(computer_guess, player_creation, filter_array)
-  computer_guess.each_with_index do |v, i|
-    player_creation.each_with_index do |x, y|
-      next unless v == x
-      break if filter_array.any? { |v1| v1[1] == y || v1[0] == i }
+        break if both_qualified(computer_guess, player_creation, filter_array) == false
 
-      return true
+        algorithm_array[i] = 2
+        filter_array.push([i, y])
+      end
     end
   end
-  false
+
+  def none_index(computer_guess, player_creation, filter_array, algorithm_array)
+    computer_guess.each_with_index do |v, i|
+      player_creation.each_with_index do |x, y|
+        next unless v != x
+
+        next if filter_array.any? { |v| v[0] == i || v[1] == y }
+
+        algorithm_array[i] = 3
+
+        filter_array.push([i, y])
+      end
+    end
+  end
+
+  def both_qualified(computer_guess, player_creation, filter_array)
+    computer_guess.each_with_index do |v, i|
+      player_creation.each_with_index do |x, y|
+        next unless v == x
+        break if filter_array.any? { |v1| v1[1] == y || v1[0] == i }
+
+        return true
+      end
+    end
+    false
+  end
 end
-
-# def player_creation_qualified(player_creation, filter_array)
-#   player_creation.each_with_index do |_x, y|
-#     p filter_array
-#     break if filter_array.any? { |v| v[1] == y }
-
-#     p 'true'
-#     true
-#   end
-#   false
-# end
-# end
-p exact_values(computer_guess, player_creation)
